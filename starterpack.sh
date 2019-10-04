6c468965dc419820c81b4700c5988028092f8222
Jinoo Lee <etture@gmail.com>
2019-10-04 05:18:45 +0000

    Added starterpack bash script for new Ubuntu container (install docker, define aliases)

---
+++ starterpack.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+echo alias ..="cd .." >> ~/.bash_aliases
+echo alias ...="cd ../.." >> ~/.bash_aliases
+echo alias ....="cd ../../.." >> ~/.bash_aliases
+echo alias .....="cd ../../../.." >> ~/.bash_aliases
+echo alias ......="cd ../../../../.." >> ~/.bash_aliases
+
+echo alias docker="sudo docker" >> ~/.bash_aliases
+echo alias ybigta-spark="docker exec -it ybigta-spark /bin/bash" >> ~/.bash_aliases
+echo alias ybigta-python="docker exec -it ybigta-python /bin/bash" >> ~/.bash_aliases
+
+echo force_color_prompt=yes >> ~/.bashrc
