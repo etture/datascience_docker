9bbe552a74c5a584199a446f981da2c5698a90b3
Jinoo Lee <etture@gmail.com>
2019-10-04 16:01:56 +0900

    Update starterpack.sh

--- starterpack.sh
+++ starterpack.sh
@@ -10,15 +10,15 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo apt-get install -y -f
 
-echo alias ..="cd .." >> ~/.bash_aliases
-echo alias ...="cd ../.." >> ~/.bash_aliases
-echo alias ....="cd ../../.." >> ~/.bash_aliases
-echo alias .....="cd ../../../.." >> ~/.bash_aliases
-echo alias ......="cd ../../../../.." >> ~/.bash_aliases
+echo alias ..='"cd .."' >> ~/.bash_aliases
+echo alias ...='"cd ../.."' >> ~/.bash_aliases
+echo alias ....='"cd ../../.."' >> ~/.bash_aliases
+echo alias .....='"cd ../../../.."' >> ~/.bash_aliases
+echo alias ......='"cd ../../../../.."' >> ~/.bash_aliases
 
-echo alias docker="sudo docker" >> ~/.bash_aliases
-echo alias ybigta-spark="docker exec -it ybigta-spark /bin/bash" >> ~/.bash_aliases
-echo alias ybigta-python="docker exec -it ybigta-python /bin/bash" >> ~/.bash_aliases
+echo alias docker='"sudo docker"' >> ~/.bash_aliases
+echo alias ybigta-spark='"docker exec -it ybigta-spark /bin/bash"' >> ~/.bash_aliases
+echo alias ybigta-python='"docker exec -it ybigta-python /bin/bash"' >> ~/.bash_aliases
 
 echo force_color_prompt=yes >> ~/.bashrc
 
